7eb6b39b5e80f34526fe6c38669eac1548f73600
Muhammadjon Abdulloev <ibnkhayrulloh@gmail.com>
2023-09-17 01:19:14 +0300

    Finished.

--- config/settings.py
+++ config/settings.py
@@ -150,8 +150,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "your_email"
-EMAIL_HOST_PASSWORD = "app_password"
+EMAIL_HOST_USER = "your_email@example.com"
+EMAIL_HOST_PASSWORD = "your_app_password"
 # To get your Gmail app password: https://support.google.com/accounts/answer/185833
 
 
